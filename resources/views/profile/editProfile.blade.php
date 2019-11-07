6690e90b7887a36118e67a68dc77fdc5b96ec043
Thibault <thibault.feugere@ynov.com>
2019-11-07 15:16:04 +0100

    :sparkles: Adding update possibilities for users

--- resources/views/profile/editProfile.blade.php
+++ resources/views/profile/editProfile.blade.php
@@ -1,29 +1,24 @@
 @extends('layouts.app')
-
 @section('content')
-<form method="post">
+<form method="post" action="{{ route('update') }}">
     <div class="form-group">
         <label for="last_name">Last Name</label>
-        <input type="text" class="form-control" id="last_name" placeholder="Enter last name" value="">
+        <input type="text" class="form-control" id="last_name" placeholder="Enter last name" name="last_name" value="{{ $profile ?? '' ? $profile->last_name : '' }}">
     </div>
     <div class="form-group">
         <label for="first_name">First Name</label>
-        <input type="text" class="form-control" id="first_name" placeholder="Enter first name" value="">
+        <input type="text" class="form-control" id="first_name" placeholder="Enter first name" name="first_name" value="{{ $profile ?? '' ? $profile->first_name : '' }}">
     </div>
     <div class="form-group">
         <label for="last_name">Date de naissance</label>
-        <input type="date" name="age" id="age" placeholder="Definissez une date de naissance" class="form-control" value="">
+        <input type="date" id="age" placeholder="Definissez une date de naissance" class="form-control" name="birthday" value="{{ $profile ?? '' ? substr($profile->dateOfBirth, 0, 10) : '' }}">
     </div>
     <div class="form-group">
         <label for="exampleInputEmail1">Adresse Email</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    </div>
-    <div class="form-group">
-        <label for="exampleInputPassword1">Mot de passe</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email" value="{{ Auth::user() ?? '' ? Auth::user()->email : '' }}">
     </div>
     <input type="hidden" name="id" value="">
     @csrf
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" class="btn btn-primary">Mettre à jour</button>
 </form>
 @endsection
--- resources/views/profile/showProfile.blade.php
+++ resources/views/profile/showProfile.blade.php
@@ -1,34 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<center>
-    <div class="container" style="border: 1px solid">
-        <div class="row profile">
-            <div class="col-md-12">
-                <div class="profile-sidebar">
-                    <!-- SIDEBAR USERPIC -->
-                    <div class="profile-userpic">
-                        <img src="//placehold.it/150" class="mx-auto img-fluid img-circle d-block" alt="avatar">
-                    </div>
-                    <!-- END SIDEBAR USERPIC -->
-                    <!-- SIDEBAR USER TITLE -->
-                    <div class="profile-usertitle">
-                        <div class="profile-usertitle-name">
-                            Marcus Doe
-                        </div>
-                        <div class="profile-usertitle-job">
-                            Developer
-                        </div>
-                    </div>
-                    <!-- END SIDEBAR USER TITLE -->
-                    <!-- SIDEBAR BUTTONS -->
-                    <div class="profile-userbuttons">
-                        <button type="button" class="btn btn-success btn-sm">Follow</button>
-                        <a href="{{ route('edit') }}"><button type="submit" class="btn btn-danger btn-sm">Éditer profil</button></a>                    </div>
-                    <!-- END SIDEBAR BUTTONS -->
+<div class="container" style="border: 1px solid">
+    <div class="row profile">
+        <div class="col-md-12">
+            <div class="profile-sidebar">
+                <!-- SIDEBAR USERPIC -->
+                <div class="profile-userpic">
+                    <img src="//placehold.it/150" class="mx-auto img-fluid img-circle d-block" alt="avatar">
                 </div>
+                <!-- END SIDEBAR USERPIC -->
+                <!-- SIDEBAR USER TITLE -->
+                <div class="profile-usertitle">
+                    <div class="profile-usertitle-name">
+                        {{ $profile ?? '' ? $profile->first_name : '' }} {{ $profile ?? '' ? $profile->last_name : '' }}
+                    </div>
+                    <div class="profile-usertitle-job">
+                        {{ $profile ?? '' ? substr($profile->dateOfBirth, 0, 10) : '' }}
+                    </div>
+                </div>
+                <!-- END SIDEBAR USER TITLE -->
+                <!-- SIDEBAR BUTTONS -->
+                <div class="profile-userbuttons">
+                    <a href="{{ route('edit') }}"><button type="submit" class="btn btn-success btn-sm">Éditer profil</button></a>
+                    <a href="{{ route('edit') }}"><button type="submit" class="btn btn-danger btn-sm">Supprimer le profil</button></a>
+                </div>
+                <!-- END SIDEBAR BUTTONS -->
             </div>
         </div>
     </div>
-</center>
+</div>
 @endsection
