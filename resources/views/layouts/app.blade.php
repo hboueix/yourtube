b1a75717af32fc6b42e4076621eb6f9adf6ec937
Thibault <thibault.feugere@ynov.com>
2019-11-08 12:43:34 +0100

    :sparkles: Improving navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,12 +57,17 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('home') }}">
+                                   Dashboard
+                                </a>
+                                <a class="dropdown-item" href="{{ route('show') }}">
+                                    Mon profil
+                                </a>
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                     {{ __('Se déconnecter') }}
                                 </a>
-
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                     @csrf
                                 </form>
