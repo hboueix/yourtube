c4576c97333dc2f8bdb66f8bd15c28d085ba4ddb
Maxime Larrieu <maximelarrieu@github.com>
2019-11-24 12:12:10 +0100

    :art: upload video form view

---
+++ resources/views/videos/uploadVideo.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Partager une vidéo') }}</div>
+                    <form method="post" action="" enctype="multipart/form-data" style="margin: 5px">
+                        <div class="custom-file">
+                            <input type="file" class="custom-file-input" id="customFile" name="miniature">
+                            <label class="custom-file-label" for="customFile">Choisir la miniature</label>
+                        </div>
+                        <div class="form-group">
+                            <label for="last_name">Titre</label>
+                            <input type="text" class="form-control" id="title" placeholder="Titre putaclic" name="title" value="{{ $profile ?? '' ? $profile->last_name : '' }}" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="first_name">Description</label>
+                            <textarea type="text" class="form-control" id="first_name" placeholder="Description" name="description" value="{{ $profile ?? '' ? $profile->first_name : '' }}" required></textarea>
+                        </div>
+                        <input type="hidden" name="id" value="">
+                        @csrf
+                        <button type="submit" class="btn btn-primary">Mettre en ligne</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
