c4b739d7238b490ea2c1ef759ffb507efdcc0176
Thibault <thibault.feugere@ynov.com>
2019-11-26 12:32:37 +0100

    :sparkles: New path and fixing bug path

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,8 +21,6 @@
                 <img src="{{asset('storage/'. $video->image)}}">
                 <div class="card-body">
                     <h5 class="card-title">{{ $video->title }}</h5>
-                    <p class="card-text">{{ $video->description }}</p>
-                    <a href="#" class="btn btn-primary">Voir la vidéo</a>
                 </div>
             </div>
             @endforeach
@@ -34,14 +32,9 @@
             <div class="row">
             @foreach($tend_videos as $t_video)
             <div class="card col-sm-4">
-                <video width="320" height="240" class="card-img-top" controls>
-                    <source src="https://www.videvo.net/videvo_files/converted/2013_07/videos/hd0079.mov26726.mp4" type="video/mp4">
-                    Your browser does not support the video tag.
-                </video>
+                <img src="{{asset('storage/'. $video->image)}}">
                 <div class="card-body">
                     <h5 class="card-title">{{ $t_video->title }}</h5>
-                    <p class="card-text">{{ $t_video->description }}</p>
-                    <a href="#" class="btn btn-primary">Voir la vidéo</a>
                 </div>
             </div>
             @endforeach
@@ -53,14 +46,9 @@
             <div class="row">
             @foreach ($rand_videos as $r_video)
             <div class="card col-sm-4">
-                <video width="320" height="240" class="card-img-top" controls>
-                    <source src="https://www.videvo.net/videvo_files/converted/2013_07/videos/hd0079.mov26726.mp4" type="video/mp4">
-                    Your browser does not support the video tag.
-                </video>
+                <img src="{{asset('storage/'. $video->image)}}">
                 <div class="card-body">
                     <h5 class="card-title">{{ $r_video->title }}</h5>
-                    <p class="card-text">{{ $r_video->description }}</p>
-                    <a href="#" class="btn btn-primary">Voir la vidéo</a>
                 </div>
             </div>
             @endforeach
