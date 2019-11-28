3a6fa2f351c0751cd57e7b0d1c8ee0b72a8d62c0
Thibault <thibault.feugere@ynov.com>
2019-11-29 00:15:19 +0100

    :lipstick: Adding share and flex for views

--- resources/views/videos/showVideo.blade.php
+++ resources/views/videos/showVideo.blade.php
@@ -7,18 +7,36 @@
             Your browser does not support the video tag.
         </video>
         <div style="margin-top: 20px">
-            <div class="text-left" style="width: 80%; float:left;">
-                <h2>{{$videos->title}}</h2>
-                <button type="button" class="btn btn-success"><i class="fas fa-thumbs-up" style="margin-right: 10px"></i>J'aime</button>
-                <button type="button" class="btn btn-danger"><i class="fas fa-thumbs-down" style="margin-right: 10px"></i>J'aime pas</button>
-            </div>
-            <div class="text-right" style="width: 20%; float: left;">
-                <h3>{{$videos->nbWatch}} vues</h3>
+            <div class="d-flex justify-content-between">
+                <div>
+                    <h2>{{$videos->title}}</h2>
+                    <button type="button" class="btn btn-success"><i class="fas fa-thumbs-up"
+                                                                     style="margin-right: 10px"></i>J'aime
+                    </button>
+                    <button type="button" class="btn btn-danger"><i class="fas fa-thumbs-down"
+                                                                    style="margin-right: 10px"></i>J'aime pas
+                    </button>
+                </div>
+                <div><h3>{{$videos->nbWatch}} vues</h3></div>
             </div>
         </div>
-        <div class="text-left" style="border: 1px solid; margin-top: 150px">
-            <img src="{{ asset('storage/' . $videos->avatar)}}" style="width: 80px; height: 80px; border-radius: 100%">
-            <h4>{{$videos->name}}</h4>
-        </div>
+        <hr>
+        <img src="{{ asset('storage/' . $videos->avatar)}}" style="width: 80px; height: 80px; border-radius: 100%">
+        <h4>{{$videos->name}}</h4>
+        Partager sur :
+        <a href="https://www.facebook.com/sharer/sharer.php?u={{route('video_show', $videos->id)}}" target="_blank">
+            Facebook
+        </a>
+        <a href="https://twitter.com/intent/tweet?text=Cette vidéo pourrait vous intéresser : {{route('video_show', $videos->id)}}"
+           target="_blank">
+            Twitter
+        </a>
+        <a href="https://www.linkedin.com/shareArticle?url={{route('video_show', $videos->id)}}" target="_blank">
+            LinkedIn
+        </a>
+        <a href="mailto:?subject={{$videos->title}}'&body=Cette vidéo pourrait vous intéresser : {{route('video_show', $videos->id)}} via Yourtube.fr"
+           target="_blank">
+            Mail
+        </a>
     </div>
 @endsection
