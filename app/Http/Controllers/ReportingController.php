abf77a34fbda525d10f556268566ac092d04e2ec
Maxime Larrieu <maximelarrieu@github.com>
2019-11-29 12:54:50 +0100

    :construction: create reporting controller and functions

---
+++ app/Http/Controllers/ReportingController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Reporting;
+use Illuminate\Http\Request;
+
+class ReportingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Reporting  $reporting
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Reporting $reporting)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Reporting  $reporting
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Reporting $reporting)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Reporting  $reporting
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Reporting $reporting)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Reporting  $reporting
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Reporting $reporting)
+    {
+        //
+    }
+}
