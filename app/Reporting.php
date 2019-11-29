6bc073587044728d28c825d8a50e744ecc6c4e4c
Maxime Larrieu <maximelarrieu@github.com>
2019-11-29 15:55:45 +0100

    :sparkles: fillable

--- app/Reporting.php
+++ app/Reporting.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reporting extends Model
 {
-    //
+    public $timestamps = true;
+
+    protected $fillable = ['reporter_id', 'video_id', 'content'];
+
 }
