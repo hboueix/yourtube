31cfefd5c742ad9ee6082af1a9a320f71dcbb0e0
Maxime Larrieu <maximelarrieu@github.com>
2019-11-29 12:55:58 +0100

    migration

---
+++ database/migrations/2019_11_28_221207_create_reportings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReportingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reportings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('video_id');
+            $table->unsignedBigInteger('reporter_id');
+            $table->text('content');
+            $table->timestamps();
+            $table->foreign('video_id')->references('id')->on('videos');
+            $table->foreign('reporter_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reportings');
+    }
+}
