7a38c085bfe66aecb694762654dc8a6420694c82
Maxime Larrieu <maximelarrieu@github.com>
2019-11-29 15:57:11 +0100

    :construction: factory default

---
+++ database/factories/ReactionsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Reactions;
+use Faker\Generator as Faker;
+
+$factory->define(Reactions::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
