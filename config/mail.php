a2187f2cdbf39159ceb0e6632bfa2f4fa34398d3
Hugo Boueix <hugo@yourtube.fr>
2019-11-30 16:40:37 +0100

    :sparkles: production + mail OK

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'no-response@yourtube.fr'),
+        'name' => env('MAIL_FROM_NAME', 'Yourtube'),
     ],
 
     /*
