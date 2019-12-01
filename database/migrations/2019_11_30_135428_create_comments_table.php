2b3cd0401e86ab9977f4182efbe112d9b9a5193a
Thibault <thibault.feugere@ynov.com>
2019-12-01 22:06:07 +0100

    :sparkles: Adding some columns

--- database/migrations/2019_11_30_135428_create_comments_table.php
+++ database/migrations/2019_11_30_135428_create_comments_table.php
@@ -15,6 +15,9 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('video_id');
+            $table->string('content');
             $table->timestamps();
         });
     }
