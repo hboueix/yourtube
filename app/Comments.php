c6d620edb74f809b00087b25ba6ea9131272cbe9
Thibault <thibault.feugere@ynov.com>
2019-12-01 22:07:14 +0100

    :sparkles: Adding fillable columns to comments

--- app/Comments.php
+++ app/Comments.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comments extends Model
 {
-    //
+    public $timestamps = true;
+
+    protected $fillable = ['user_id', 'video_id', 'content'];
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
 }
