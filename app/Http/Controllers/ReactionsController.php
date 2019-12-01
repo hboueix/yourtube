b6ec9781f1644e767b327b1116752ec961456646
Thibault <thibault.feugere@ynov.com>
2019-12-01 23:45:41 +0100

    :sparkles: Adding like and dislike functions

--- app/Http/Controllers/ReactionsController.php
+++ app/Http/Controllers/ReactionsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Reactions;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class ReactionsController extends Controller
 {
@@ -30,7 +32,7 @@ class ReactionsController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -41,7 +43,7 @@ class ReactionsController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Reactions  $reactions
+     * @param \App\Reactions $reactions
      * @return \Illuminate\Http\Response
      */
     public function show(Reactions $reactions)
@@ -52,7 +54,7 @@ class ReactionsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Reactions  $reactions
+     * @param \App\Reactions $reactions
      * @return \Illuminate\Http\Response
      */
     public function edit(Reactions $reactions)
@@ -63,8 +65,8 @@ class ReactionsController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Reactions  $reactions
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Reactions $reactions
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Reactions $reactions)
@@ -75,11 +77,75 @@ class ReactionsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Reactions  $reactions
+     * @param \App\Reactions $reactions
      * @return \Illuminate\Http\Response
      */
     public function destroy(Reactions $reactions)
     {
         //
     }
+
+    public function like($id)
+    {
+        $auth_id = Auth::id();
+        $reaction = DB::table('reactions')->where([
+            'user_id' => $auth_id,
+            'video_id' => $id
+        ])->first();
+        if ($reaction == null) {
+            DB::table('reactions')->insert([
+                'user_id' => $auth_id,
+                'video_id' => $id,
+                'is_liked' => 1,
+                'created_at' => date('y-m-d h:m:s'),
+                'updated_at' => date('y-m-d h:m:s')
+            ]);
+            return redirect()->route('video_show', $id)->with('video_liked', true);
+        }
+        elseif ($reaction->is_liked == 0) {
+            DB::table('reactions')->where([
+                'user_id' => $auth_id,
+                'video_id' => $id
+            ])->update([
+                'is_liked' => 1,
+                'updated_at' => date('y-m-d h:m:s')
+            ]);
+        }
+        else {
+            return redirect()->route('video_show', $id)->with('video_liked_error', true);
+        }
+    }
+
+    public function dislike($id)
+    {
+        $auth_id = Auth::id();
+        $reaction = DB::table('reactions')->where([
+            'user_id' => $auth_id,
+            'video_id' => $id
+        ])->first();
+        if ($reaction == null) {
+            DB::table('reactions')->insert([
+                'user_id' => $auth_id,
+                'video_id' => $id,
+                'is_liked' => 0,
+                'created_at' => date('y-m-d h:m:s'),
+                'updated_at' => date('y-m-d h:m:s')
+            ]);
+            return redirect()->route('video_show', $id)->with('video_disliked', true);
+        }
+        elseif ($reaction->is_liked == 1) {
+            DB::table('reactions')->where([
+                'user_id' => $auth_id,
+                'video_id' => $id
+            ])->update([
+                'is_liked' => 0,
+                'updated_at' => date('y-m-d h:m:s')
+            ]);
+            return redirect()->route('video_show', $id)->with('video_disliked', true);
+        }
+        else {
+            return redirect()->route('video_show', $id)->with('video_disliked_error', true);
+        }
+    }
+
 }
