1d05d78f6f250cd46988e4cfe94658bb21b6d3e7
Thibault <thibault.feugere@ynov.com>
2019-12-01 22:58:49 +0100

    :speech_ballon: Translating

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nom d\'utilisateur') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Pseudonyme') }}</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
