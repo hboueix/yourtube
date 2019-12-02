b48a75d68da093573c2781b0250e7a9fb989db76
Maxime Larrieu <maximelarrieu@github.com>
2019-12-02 14:31:35 +0100

    :art: alert to deleted account

--- resources/views/profile/showAllProfile.blade.php
+++ resources/views/profile/showAllProfile.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
+    @if (session('account_deleted'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            Compte supprimé avec succès !
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
