9d7576f523e4840bf7337acf721a48a95504269d
Maxime Larrieu <maximelarrieu@github.com>
2019-12-03 12:28:03 +0100

    :art: logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
-                <img src="{{ asset('storage/logo_200x122.png') }}" style="width: 100px; height: 61px"> 
+                <img src="{{ asset('storage/logo_200x122.png') }}" style="width: 100px">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false"
