addb309ca9336a8bf09665d811630f5e61dcb6e5
Thibault <thibault.feugere@ynov.com>
2019-12-12 18:24:07 +0100

    :lipstick: Adding icons & CTRL + ALT + SUPP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,55 +21,58 @@
         <div class="card-body">
             <h4>Récemments publiées</h4>
             <hr/>
-                <div class="row">
-                    @foreach($videos as $video)
-                        <div class="card col-sm-4">
-                            <div
-                                style="width: 100%; height: 200px; background-image: url('{{asset('storage/'. $video->miniature)}}'); background-position: center; background-size: cover">
-                            </div>
-                            <div class="card-body">
-                                <a href="{{ route('video_show', $video->id) }}"
-                                   style="text-decoration: none; color: inherit"><h5
-                                        class="card-title">{{ $video->title }}</h5></a>
-                                <span><i class="fas fa-eye" style="margin-right: 10px"></i>{{$video->nbWatch}}</span>
+            <div class="row">
+                @foreach($videos as $video)
+                    <div class="card col-sm-4">
+                        <div
+                            style="width: 100%; height: 200px; background-image: url('{{asset('storage/'. $video->miniature)}}'); background-position: center; background-size: cover">
+                        </div>
+                        <div class="card-body">
+                            <a href="{{ route('video_show', $video->id) }}"
+                               style="text-decoration: none; color: inherit"><h5
+                                    class="card-title">{{ $video->title }}</h5></a>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <span class="badge badge-pill badge-info">Catégorie</span>
+                                <span><i class="fas fa-eye mr-1"></i>{{$video->nbWatch}}</span>
                             </div>
                         </div>
-                    @endforeach
+                    </div>
+                @endforeach
             </div>
             <h4>Vidéos tendances</h4>
             <hr/>
-                <div class="row">
-                    @foreach($tend_videos as $t_video)
-                        <div class="card col-sm-4">
-                            <div
-                                style="width: 100%; height: 200px; background-image: url('{{asset('storage/'. $t_video->miniature)}}'); background-position: center; background-size: cover">
-                            </div>
-                            <div class="card-body">
-                                <a href="{{ route('video_show', $t_video->id) }}"
-                                   style="text-decoration: none; color: inherit"><h5
-                                        class="card-title">{{ $t_video->title }}</h5></a>
-                                <h6>{{$t_video->nbWatch}} vues</h6>
-                            </div>
+            <div class="row">
+                @foreach($tend_videos as $t_video)
+                    <div class="card col-sm-4">
+                        <div
+                            style="width: 100%; height: 200px; background-image: url('{{asset('storage/'. $t_video->miniature)}}'); background-position: center; background-size: cover">
                         </div>
-                    @endforeach
-                </div>
+                        <div class="card-body">
+                            <a href="{{ route('video_show', $t_video->id) }}"
+                               style="text-decoration: none; color: inherit"><h5
+                                    class="card-title">{{ $t_video->title }}</h5></a>
+                            <h6>{{$t_video->nbWatch}} vues</h6>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
             <h4>Toutes les vidéos</h4>
             <hr/>
-                <div class="row">
-                    @foreach ($rand_videos as $r_video)
-                        <div class="card col-sm-4">
-                            <div
-                                style="width: 100%; height: 200px; background-image: url('{{asset('storage/'. $r_video->miniature)}}'); background-position: center; background-size: cover">
-                            </div>
-                            <div class="card-body">
-                                <a href="{{ route('video_show', $r_video->id) }}"
-                                   style="text-decoration: none; color: inherit"><h5
-                                        class="card-title">{{ $r_video->title }}</h5></a>
-                                <h6>{{$r_video->nbWatch}} vues</h6>
-                            </div>
+            <div class="row">
+                @foreach ($rand_videos as $r_video)
+                    <div class="card col-sm-4">
+                        <div
+                            style="width: 100%; height: 200px; background-image: url('{{asset('storage/'. $r_video->miniature)}}'); background-position: center; background-size: cover">
                         </div>
-                    @endforeach
-                </div>
+                        <div class="card-body">
+                            <a href="{{ route('video_show', $r_video->id) }}"
+                               style="text-decoration: none; color: inherit"><h5
+                                    class="card-title">{{ $r_video->title }}</h5></a>
+                            <h6>{{$r_video->nbWatch}} vues</h6>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </div>
 @endsection
