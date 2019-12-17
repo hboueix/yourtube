844ff508b7c95afcd358aa39d478a0b66cc883fd
Maxime Larrieu <maximelarrieu@github.com>
2019-12-17 09:51:47 +0100

    :art: new tab users in admin page

--- resources/views/admin/showAdmin.blade.php
+++ resources/views/admin/showAdmin.blade.php
@@ -17,7 +17,7 @@
         </div>
     @endif
     <div class="container">
-        <div class="row justify-content-center" style="margin-bottom: 20px">
+        <div class="row justify-content-center mb-3">
             <div class="col-md-8">
                 <div class="card table-responsive">
                     <div class="card-header">Liste signalements</div>
@@ -58,7 +58,7 @@
                 </div>
             </div>
         </div>
-        <div class="row justify-content-center">
+        <div class="row justify-content-center mb-3">
             <div class="col-md-8">
                 <div class="card table-responsive">
                     <div class="card-header">Liste commentaires</div>
@@ -89,6 +89,44 @@
                     @if (sizeof($comments) == 0)
                         Rien à signaler
                     @endif
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card table-responsive">
+                    <div class="card-header">Liste utilisateurs</div>
+                    <div class="card-body">
+                        @foreach ($profile as $profiler)
+                            <div class="media">
+                                <img src="{{ asset('storage/' . $profiler->avatar) }}" class="mr-3" alt="miniature"
+                                     style="height: 120px; width: 120px">
+                                <div class="media-body" style="text-overflow:  ellipsis;  overflow: hidden !important;">
+                                    <h5 class="mt-1"> | {{ $profiler->name }}</h5>
+                                    <h6 class="mt-1">{{ $profiler->email }}</h6>
+                                    <p>
+                                        {{ $profiler->dateOfBirth }}
+                                    </p>
+                                    <p>
+                                        Crée le : {{ $profiler->created_at }}
+                                    </p>
+                                </div>
+                                <div class="text-center" style="width: 15%;">
+                                    <a href="{{ route('profile_show', $profiler->name) }}">
+                                        <button type="button" class="btn btn-success" style=" margin: 5px">
+                                            <i class="fas fa-user"></i>
+                                        </button>
+                                    </a>
+                                    <a href="{{ route('admin_profile_destroy', $profiler->id) }}">
+                                        <button type="button" class="btn btn-danger">
+                                            <i class="fas fa-trash-alt"></i>
+                                        </button>
+                                    </a>
+                                </div>
+                            </div>
+                            <hr/>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
