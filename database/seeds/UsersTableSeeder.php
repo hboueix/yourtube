f6947254e530c63f22e00c4ac9bf50c74a85b395
Thibault <thibault.feugere@ynov.com>
2020-01-10 14:33:19 +0100

    :sparkles: Removing videos seeding because of moderation

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,22 +14,16 @@ class UsersTableSeeder extends Seeder
         factory(App\User::class, 1)->create()->each(function ($user) {
             $user->assignRole('administrateur');
             $user->profile()->save(factory(App\Profile::class)->make());
-            $videos = factory(App\Videos::class, 5)->make();
-            $user->videos()->saveMany($videos);
         });
 
         factory(App\User::class, 1)->create()->each(function ($user) {
             $user->assignRole('moderateur');
             $user->profile()->save(factory(App\Profile::class)->make());
-            $videos = factory(App\Videos::class, 5)->make();
-            $user->videos()->saveMany($videos);
         });
 
         factory(App\User::class, 1)->create()->each(function ($user) {
             $user->assignRole('yourtubeur');
             $user->profile()->save(factory(App\Profile::class)->make());
-            $videos = factory(App\Videos::class, 5)->make();
-            $user->videos()->saveMany($videos);
         });
     }
 }
