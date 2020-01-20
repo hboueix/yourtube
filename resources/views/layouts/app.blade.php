e03db80e8acdcb7b61ccd359f83b7a986b271243
Thibault <thibault.feugere@ynov.com>
2020-01-20 19:33:36 +0100

    :lipstick: Fixing carret bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,13 +74,14 @@
                         @else
                         <img src="https://static.asianetnews.com/img/default-user-avatar.png" width="40" height="40" style="border-radius: 100%">
                         @endif
-                            <li class="nav-item dropdown mr-1">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggleavatar" href="#" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ ucfirst(Auth::user()->name) }} <span class="caret"></span>
-                            </a>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{ route('home') }}">
                                     Dashboard
                                 </a>
