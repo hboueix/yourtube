09ca5d7288b3d5b8fc31494db50d22373ce87ceb
Thibault <thibault.feugere@ynov.com>
2020-01-20 19:59:59 +0100

    :lipstick: Adding better look on button

--- resources/views/profile/showProfile.blade.php
+++ resources/views/profile/showProfile.blade.php
@@ -23,9 +23,11 @@
                     <!-- SIDEBAR USERPIC -->
                     <div class="profile-userpic">
                         @if(strlen($profile->avatar) > 0)
-                            <img src="{{ asset('storage/'. $profile->avatar) }}" style="width: 250px; height: 250px; border-radius: 100%">
+                            <img src="{{ asset('storage/'. $profile->avatar) }}"
+                                 style="width: 250px; height: 250px; border-radius: 100%">
                         @else
-                            <img src="https://static.asianetnews.com/img/default-user-avatar.png" style="width: 250px; height: 250px">
+                            <img src="https://static.asianetnews.com/img/default-user-avatar.png"
+                                 style="width: 250px; height: 250px">
                         @endif
                     </div>
                     <br>
@@ -41,53 +43,68 @@
                     <!-- SIDEBAR BUTTONS -->
                     @if($profile->id == \Illuminate\Support\Facades\Auth::id())
                         <div>
-                            <a href="{{ route('profile_edit') }}"><button type="submit" class="btn btn-success btn-sm"><i class="fas fa-pencil-alt" style="margin-right: 5px"></i>Éditer profil</button></a>
+                            <a href="{{ route('profile_edit') }}">
+                                <button type="submit" class="btn btn-success btn-sm"><i class="fas fa-pencil-alt"
+                                                                                        style="margin-right: 5px"></i>Éditer
+                                    profil
+                                </button>
+                            </a>
                         </div>
-                    @endif
-                    <!-- END SIDEBAR BUTTONS -->
+                @endif
+                <!-- END SIDEBAR BUTTONS -->
                 </div>
             </div>
             <h3>Vidéos publiées</h3>
-            <hr />
+            <hr/>
             @if(sizeof($videos) == 0)
                 <div class="alert alert-warning" role="alert">
-                    Vous n'avez pas publié de vidéo, <a href="{{ route('video_form') }}">publiez votre permière vidéo</a> !
+                    Vous n'avez pas publié de vidéo, <a href="{{ route('video_form') }}">publiez votre permière
+                        vidéo</a> !
                 </div>
             @endif
             @foreach ($videos as $video)
-            <div class="media">
-                <div class="mr-3" style="width: 400px; height: 200px; background-image: url('{{asset('storage/'. $video->miniature)}}'); background-position: center; background-size: cover"></div>
-                <div class="media-body" style="text-overflow:  ellipsis;  overflow: hidden !important;">
-                    <a href="{{route('video_show', $video->id)}}"><h5 class="mt-1">{{ $video->title }}</h5></a>
-                    <p>
-                        {{ $video->description }}
-                    </p>
-                    <p>
-                        {{ $video->created_at }}
-                    </p>
-                </div>
-                {{--@php(dd($video));--}}
-                <div class="text-center" style="width: 20%">
-                    <button type="button" class="btn btn-success" style=" margin-bottom: 5px">
-                        <i class="fas fa-thumbs-up" style="margin-right: 10px"></i><span>{{ $video->likes }}</span>
-                    </button>
-                    <button type="button" class="btn btn-danger" style=" margin-bottom: 5px">
-                        <i class="fas fa-thumbs-down" style="margin-right: 10px"></i><span>{{ $video->dislikes }}</span>
-                    </button>
-                    <button type="button" class="btn btn-secondary" style="margin-bottom: 30px">
-                        <i class="fas fa-eye" style="margin-right: 10px"></i><span>{{ $video->nbWatch }}</span>
-                    </button>
-                    @if($profile->id == \Illuminate\Support\Facades\Auth::id())
-                        <div class="profile-userbuttons" style="margin-bottom: 5px">
-                            <a href="{{ route('video_edit', $video->id) }}"><button type="submit" class="btn btn-info btn-sm"><i class="fas fa-pencil-alt" style="margin-right: 5px"></i>Modifier</button></a>
+                <div class="media d-flex align-content-center flex-wrap">
+                    <a href="{{route('video_show', $video->id)}}">
+                        <div class="mr-3"
+                             style="width: 300px; height: 200px; background-image: url('{{asset('storage/'. $video->miniature)}}'); background-position: center; background-size: cover">
                         </div>
-                        <div class="profile-userbuttons">
-                            <a href="{{ route('video_destroy', $video->id) }}"><button type="submit" class="btn btn-danger btn-sm" style="vertical-align: bottom"><i class="fas fa-trash-alt" style="margin-right: 10px"></i>Supprimer</button></a>
+                    </a>
+                    <div class="media-body">
+                        <a href="{{route('video_show', $video->id)}}"><h5 class="mt-1">{{ $video->title }}</h5></a>
+                        <p>
+                            {{ $video->description }}
+                        </p>
+                        <p>
+                            {{ $video->created_at }}
+                        </p>
+                    </div>
+
+                    {{--@php(dd($video));--}}
+                    <div class="text-center">
+                        <div style="font-size: 20px;">
+                            <span class="badge badge-secondary"> <i class="fas fa-thumbs-up"
+                                                                    style="margin-right: 10px"></i><span>{{ $video->likes }}</span></span>
+                            <span class="badge badge-secondary"> <i class="fas fa-thumbs-down"
+                                                                    style="margin-right: 10px"></i><span>{{ $video->dislikes }}</span></span>
+                            <span class="badge badge-secondary">  <i class="fas fa-eye"
+                                                                     style="margin-right: 10px"></i><span>{{ $video->nbWatch }}</span></span>
                         </div>
-                    @endif
+                        <br>
+                        @if($profile->id == \Illuminate\Support\Facades\Auth::id())
+                            <a href="{{ route('video_edit', $video->id) }}">
+                                <button type="submit" class="btn btn-outline-dark btn-sm"><i
+                                        class="fas fa-pencil-alt"></i>
+                                </button>
+                            </a>
+                            <a href="{{ route('video_destroy', $video->id) }}">
+                                <button type="submit" class="btn btn-danger btn-sm">
+                                    <i class="fas fa-trash-alt"></i>
+                                </button>
+                            </a>
+                        @endif
+                    </div>
                 </div>
-            </div>
-            <hr />
+                <hr/>
             @endforeach
         </div>
     </div>
