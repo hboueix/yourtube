7a306d98ef5cd4487cca8da9a280a72d70f2fe70
Hugo Boueix <hboueix99@gmail.com>
2020-01-24 01:03:24 +0100

    :art: view search results w/ profile & videos

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -7,44 +7,45 @@
         <hr/>
         <div class="row">
             @foreach ($profiles as $profiler)
-                <div class="media">
+                <div class="card col align-self-center align-items-center mb-3">
+                    <a href="{{ route('profile_show', $profiler->name) }}" style="text-decoration: none; color: inherit">
                     @if(strlen($profiler->avatar) > 0)
-                        <img src="{{ asset('storage/' . $profiler->avatar) }}" class="mr-3" alt="miniature"
+                        <img src="{{ asset('storage/' . $profiler->avatar) }}" class="mt-3" alt="miniature" style="border-radius: 100%;"
                                 width="80">
                     @else
-                        <img src="https://static.asianetnews.com/img/default-user-avatar.png"
-                                width="80" style="margin-right: 20px">
+                        <img src="https://static.asianetnews.com/img/default-user-avatar.png" class="mt-3" alt="miniature" style="border-radius: 100%;"
+                                width="80">
                     @endif
-                    <div class="media-body" style="text-overflow:  ellipsis;  overflow: hidden !important;">
-                        <h5 class="mt-1">{{ $profiler->name }}</h5>
-
-                        <h6 class="mt-1">{{ $profiler->email }}</h6>
-                        <p>
-                            {{ $profiler->dateOfBirth }}
-                        </p>
-                        <p>
-                            Crée le : {{ $profiler->created_at }}
-                        </p>
+                    <div style="text-align:center; text-overflow:  ellipsis;  overflow: hidden !important;">
+                        <h5 class="mt-1"><b>{{ $profiler->name }}</b></h5>
+                        </a>
+                        @if($profiler->subscribers > 0)
+                            @if($profiler->subscribers == 1)
+                            <p>{{ $profiler->subscribers }} abonné</p>
+                            @else
+                            <p>{{ $profiler->subscribers }} abonnés</p>
+                            @endif
+                        @else
+                            <p>Pas d'abonné</p>
+                        @endif
                     </div>
                 </div>
                 <hr/>
             @endforeach
             @foreach($videos as $video)
             <div class="col-sm-12">
-                <div class="col-sm-4">
+                <div class="card w-100 row" style="display: inline-block;">
                     <a href="{{ route('video_show', $video->id) }}"
                         style="text-decoration: none; color: inherit">
-                        <div
-                            style="width: 100%; height: 200px; background-image: url('{{asset('storage/'. $video->miniature)}}'); background-position: center; background-size: cover">
-                        </div>
+                            <img class="card-img-left" style="float:left;" src="{{asset('storage/'. $video->miniature)}}"></img>
                         <div class="card-body">
                             <h5 class="card-title">{{ $video->title }}</h5>
+                            <span><i class="fas fa-eye mr-1"></i>{{$video->nbWatch}}</span>
+                        </div>
+                        <div>
+                            <p>{{ $video->description }}</p>
                         </div>
                     </a>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <span class="badge badge-pill badge-info">{{$video->category_name}}</span>
-                        <span><i class="fas fa-eye mr-1"></i>{{$video->nbWatch}}</span>
-                    </div>
                 </div>
             </div>
             @endforeach
