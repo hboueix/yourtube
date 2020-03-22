405fe827841b571b4bba7948ac3cd9c32dd33681
Thibault <thibault.feugere@ynov.com>
2020-03-22 16:27:04 +0100

    :lipstick: Adding ucfirst on role name

--- www/resources/views/admin/showAdmin.blade.php
+++ www/resources/views/admin/showAdmin.blade.php
@@ -211,11 +211,10 @@
                                 <div class="text-center" style="width: 30%;">
                                     <form method="post" action="{{route('role_update', $profiler->id)}}">
                                         <div class="form-group">
-                                            <label for="exampleFormControlSelect1">Éditer le rôle</label>
                                             <select class="form-control d-inline" id="exampleFormControlSelect1"
                                                     style="width: 70%" name="roles">
                                                 @foreach($roles as $role)
-                                                    <option value="{{ $role->id }}">{{ $role->name }}</option>
+                                                    <option value="{{ $role->id }}">{{ ucfirst($role->name) }}</option>
                                                 @endforeach
                                             </select>
                                             <input type="hidden" name="id" value="">
