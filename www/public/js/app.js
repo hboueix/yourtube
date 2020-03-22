78897e1cd0714fc59105cd1b49f174712f0f4d56
Thibault <thibault.feugere@ynov.com>
2020-03-22 16:06:34 +0100

    :pencil: Adding default configuration

--- www/public/js/app.js
+++ www/public/js/app.js
@@ -49506,8 +49506,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/html/yourtube/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /var/www/html/yourtube/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/html/yourtube/www/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/yourtube/www/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
