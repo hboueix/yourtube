06ca5ed5485854192f6db9034b1cf59335d0a001
Hugo Boueix <hboueix99@gmail.com>
2020-03-25 00:01:45 +0100

    :whale: change structure

---
+++ bin/webserver/init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Create a .env file
+cp /var/www/html/.env.default /var/www/html/.env
+
+# Composer install
+#cd /var/www/html && composer install
+
+# Change www folder user to apache user
+chown -R www-data:www-data /var/www/html
+
+# Generate app encryption key
+php artisan key:generate
+
+# Run migrations
+#php artisan migrate:refresh --seed
+
+# Start apache in foreground
+/usr/sbin/apache2ctl -D FOREGROUND
