ae4888e5eadff391183fb134a144a2c0f0391769
Hugo Boueix <hboueix99@gmail.com>
2020-03-31 16:26:30 +0200

    :whale: get back to www owned by root

--- bin/webserver/init.sh
+++ bin/webserver/init.sh
@@ -7,7 +7,7 @@ cp /var/www/html/.env.default /var/www/html/.env
 cd /var/www/html && composer install
 
 # Change www folder user to apache user
-#chown -R www-data:www-data /var/www/html
+chown -R www-data:www-data /var/www/html
 
 # Generate app encryption key
 php artisan key:generate
